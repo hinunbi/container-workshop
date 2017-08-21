efdbb5444fa36419377f5bfbc66134303ffdf6b9
hinunbi <hinunbi@gmail.com>
2017-08-21 21:20:02 +0900

    수정

--- lab04/cats.sh
+++ lab04/cats.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Cats 컨테이너 실행
 docker run --name cats -it --rm -p 8080:8080 \
--e activemq.service.host=jcha-OSX.local \
--e activemq.service.port=61616 \
--e mysql.service.host=${HOST_IP} \
--e mysql.service.port=3306 \
+-e $ACTIVEMQ_SERVICE_HOST=jcha-OSX.local \
+-e $ACTIVEMQ_SERVICE_PORT=61616 \
+-e $MYSQL_SERVICE_HOST=$HOST_IP \
+-e $MYSQL_SERVICE_PORT=3306 \
 cats
