bcd2011333ffb83a56cc854a926aa14c05b86982
hinunbi <hinunbi@gmail.com>
2017-08-23 19:36:39 +0900

    수정

--- lab04/cats.sh
+++ lab04/cats.sh
@@ -3,6 +3,6 @@
 docker run --name docker-cats -it --rm -p 8080:8080 \
 -e ACTIVEMQ_BROKER_URL=ssl://broker-amq-tcp-ssl-container-workshop.apps.13.124.168.242.nip.io:443 \
 -e ACTIVEMQ_SERVICE_PORT=61616 \
--e MYSQL_SERVICE_HOST=$HOST_IP \
+-e MYSQL_SERVICE_HOST=$(hostname --ip-address) \
 -e MYSQL_SERVICE_PORT=3306 \
 cats
