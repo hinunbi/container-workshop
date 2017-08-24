44ba922929abd4cba8dba92ea3e7d505816959c7
hinunbi <hinunbi@gmail.com>
2017-08-24 13:53:17 +0900

    수정

--- lab04/cats.sh
+++ lab04/cats.sh
@@ -5,5 +5,5 @@ docker run --name docker-cats -it --net=host --rm -p 8080:8080 \
 -e ACTIVEMQ_SERVICE_PORT=61616 \
 -e MYSQL_SERVICE_HOST=$(hostname --ip-address) \
 -e MYSQL_SERVICE_PORT=3306 \
--e _JAVA_OPTIONS=-Djava.net.preferIPv4Stack=true
+-e _JAVA_OPTIONS=-Djava.net.preferIPv4Stack=true \
 cats
